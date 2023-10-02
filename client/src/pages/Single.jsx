46e43d04c90bcf1bceef367e431d4c30d636a65c
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-10-01 23:27:40 -0500

    ajustes en el fetch axios de la data de un post por su id

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import Edit from '../assets/img/edit.png'
 import Delete from '../assets/img/delete.png'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
@@ -8,7 +8,8 @@ import moment from 'moment'
 import { AuthContext } from '../context/authContext'
 
 const Single = () => {
-  const [post, setPost] = useState({});
+  const postRef = useRef();
+  const [loading, setLoading] = useState(true);
   const location = useLocation();
   const navigate = useNavigate();
   const { currentUser } = useContext(AuthContext);
@@ -18,13 +19,14 @@ const Single = () => {
       const id = location.pathname.split("/")[2];
       try {
         const response = await axios.get(`http://localhost:8800/api/posts/${id}`);
-        setPost(response.data);
+        postRef.current = response.data[0];
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
     }
     fetchData();
-  }, []);
+  }, [location.pathname]);
 
   const handleDelete = async () => {
     try {
@@ -38,19 +40,29 @@ const Single = () => {
   const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html");
     return doc.body.textContent;
-}
+  }
+
+  const post = postRef.current;
+  
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+  
+  if (!post) {
+    return <div>No hay datos para mostrar.</div>;
+  }
 
   return (
     <div className="single">
       <div className="content">
         <img src={`../uploads/${post?.img}`} alt="" />
         <div className="user">
-          {post.userImg && <img src={post.userImg} alt="" />}
+          {post?.userImg && <img src={post?.userImg} alt="" />}
           <div className="info">
             <span>{post.username}</span>
             <p>Posteado hace {moment(post.date).fromNow()}</p>
           </div>
-          {currentUser .username === post.username && <div className="edit">
+          {currentUser.username === post.username && <div className="edit">
             <Link to="/write?edit=2" state={post}>
               <img src={Edit} alt="" />
             </Link>
