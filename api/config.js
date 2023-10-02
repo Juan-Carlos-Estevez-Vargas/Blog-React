90f9eddad52707e31c221b0e6e850023147a76d5
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-10-02 14:02:17 -0500

    variable de entorno front

--- api/config.js
+++ api/config.js
@@ -4,4 +4,4 @@ export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";
 export const DB_NAME = process.env.DB_NAME || "reactblog";
 export const DB_PORT = process.env.DB_PORT || 3306;
-export const FRONT_URL = process.env.FRONT_URL || "http://localhost:5173";
+export const FRONT_URL = "https://blog-react-node.netlify.app";
