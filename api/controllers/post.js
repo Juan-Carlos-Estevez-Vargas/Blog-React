b602ac1e66ab6e113e21975eb17bb3eb388d1287
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-10-01 19:45:18 -0500

    funcionalidad para obtener los posts desde el backend

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,20 @@
-export const addPost = (request, response) => {
-  response.json("POST");
+import { db } from "../db.js";
+
+export const getPosts = (request, response) => {
+  const query = request.query.categoria
+    ? "SELECT * FROM posts WHERE category = ?"
+    : "SELECT * FROM posts";
+
+    db.query(query, [request.query.categoria], (error, data) => {
+      if (error) response.status(500).json({ error });
+      response.status(200).json(data);
+    })
 };
+
+export const getPost = (request, response) => {};
+
+export const addPost = (request, response) => {};
+
+export const deletePost = (request, response) => {};
+
+export const updatePost = (request, response) => {};
